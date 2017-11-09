0f6893111955c4718f672a8cc774f6ca57b5dbc8
huskyhoochu <dfg1499@gmail.com>
2017-11-09 13:56:26 +0900

    generic view

--- instagram/post/apis.py
+++ instagram/post/apis.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 
-from rest_framework import status
+from rest_framework import status, mixins, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,19 +8,34 @@ from post.serializers import PostSerializer
 from .models import Post
 
 
-class PostList(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class PostList(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
+    # generics.GenericAPIView와 mixins를 상속받아 처리
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            # 포스트맨에서 파일 올릴 때 author가 생긴다
-            serializer.save(author=self.request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    # def get(self, request):
+    #     posts = Post.objects.all()
+    #     serializer = PostSerializer(posts, many=True)
+    #     return Response(serializer.data)
+    #
+    # def post(self, request):
+    #     serializer = PostSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         # 포스트맨에서 파일 올릴 때 author가 생긴다
+    #         serializer.save(author=self.request.user)
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostDetail(APIView):
