2a162d522b7903ba56561991f8d40efdfa05768e
huskyhoochu <dfg1499@gmail.com>
2017-11-16 17:47:12 +0900

    elasticbeanstalk healthcheck 대비 코드 추가

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -74,6 +74,39 @@ ALLOWED_HOSTS = [
     'api.huskyhoochu.com',
 ]
 
+# elasticbeanstalk healthcheck 대비 코드
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
+
+
 # 프론트에서 요청이 들어올 때 장고가 허용해주는 도메인들
 CORS_ORIGIN_WHITELIST = (
     'localhost:3001',
