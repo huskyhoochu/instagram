a73a3322eb06ca8017a4524a64ee1e13c7909d47
huskyhoochu <dfg1499@gmail.com>
2017-11-16 11:19:38 +0900

    sms

--- instagram/member/backends.py
+++ instagram/member/backends.py
@@ -11,6 +11,8 @@ class FacebookBackend:
             return None
 
     def get_user(self, user_id):
+        # 이게 뭐지?
+        # authentication을 커스텀할 때 반드시 필요!
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
